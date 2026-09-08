02006c6c230f0f20767505e1a9444a2e08fcc6fa
tommydev69 <sunnevian4life@gmail.com>
2026-09-08 16:13:41 +0100

    buyers

---
+++ app/buyers project/BuyerNavbar/BuyerContent.tsx
@@ -0,0 +1,41 @@
+import { link } from "fs";
+
+const BuyerContent = () => {
+    const navContent =[
+        {
+            id:1,
+            content:"home",
+            linking:'/'
+        },
+        {
+            id:2,
+            content:'shop',
+            linking:'/shop'
+
+        },
+        {
+            id:3,
+            content: 'pages',
+            linking: '/page'
+        },
+         {
+            id:4,
+            content:'blog',
+            linking:'/blog'
+         },
+         {
+            id:5,
+            content:'about us',
+            linking:'/about us'
+         },
+         {
+            id:6,
+            content:'contact us',
+            linking:'/contact us'
+         }
+    ]
+       
+    return (  );
+}
+ 
+export default BuyerContent;
